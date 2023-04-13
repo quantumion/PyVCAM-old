a836ef4a260ffd837aefa31943053c2587474c38
Kevin Chen <k377chen@uwaterloo.ca>
2023-04-13 13:14:11 -0400

    removed console output spam

--- src/pyvcam/__main__.py
+++ src/pyvcam/__main__.py
@@ -14,22 +14,27 @@ QuantumIon
 from sipyco.pc_rpc import simple_server_loop
 from sipyco import common_args
 import argparse
-import logging
+import logging, logging.config
 from pyvcam.driver import PyVCAM
 
 
 logger = logging.getLogger('pyvcam')
 logger.setLevel(logging.DEBUG)
+logger.propagate = False  # prevents logging output flooding console
+
 # create file handler which logs even debug messages
-fh = logging.FileHandler('spam.log')
+fh = logging.FileHandler('logfile.log')
 fh.setLevel(logging.DEBUG)
+
 # create console handler with a higher log level
 ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
+ch.setLevel(logging.DEBUG)
+
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
+
 # add the handlers to the logger
 logger.addHandler(fh)
 logger.addHandler(ch)
